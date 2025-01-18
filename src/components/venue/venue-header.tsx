3c81c322b8771222a13c6b608ec069c40a7184b8
klee0126@student.monash.edu <klee0126@student.monash.edu>
2025-01-18 23:13:54 +1100

    fix: commented out additional html/css

--- src/components/venue/venue-header.tsx
+++ src/components/venue/venue-header.tsx
@@ -5,7 +5,7 @@ const VenueHeader = () => {
         <header className="venue-header-container">
             <h1 className="venue-header-title">Venue Manager</h1>
             
-            <div className="venue-header-button-group">
+            {/* <div className="venue-header-button-group">
                 <button className={`${"venue-header-button"} ${"venue-header-preview-button"}`}>
                     Preview Venues
                 </button>
@@ -13,7 +13,7 @@ const VenueHeader = () => {
                 <button className={`${"venue-header-button"} ${"venue-header-qr-button"}`}>
                     QR Code
                 </button>
-            </div>
+            </div> */}
         </header>
     )
 };
